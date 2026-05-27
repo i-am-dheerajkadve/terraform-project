791a552528bd6e9dda29b2ef48df5cd6e60e17e8
Dheeraj kadve <dheerajkadve@gmail.com>
2026-05-27 22:57:15 +0530

    Update userdata.sh

--- userdata.sh
+++ userdata.sh
@@ -10,3 +10,5 @@ systemctl enable docker
 curl -L "https://github.com/docker/compose/releases/download/v2.27.0/docker-compose-linux-x86_64" -o /usr/local/bin/docker-compose
 
 chmod +x /usr/local/bin/docker-compose
+sudo apt update
+sudo apt install postgresql-client -y
